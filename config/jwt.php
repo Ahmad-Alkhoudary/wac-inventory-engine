1ae3892c5dc87c6018b3ebfe7295ee1101ea9f43
Ahmad Alkhoudary <ahmedalkhoudary@gmail.com>
2026-08-15 14:47:04 +0800

    Add required config for JWT auth

---
+++ config/jwt.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    'secret' => env('JWT_SECRET', 'secret_jwt_key_wac_inventory_engine_2026_super_secure_key_32chars'),
+
+    'keys' => [
+        'public' => env('JWT_PUBLIC_KEY'),
+        'private' => env('JWT_PRIVATE_KEY'),
+        'passphrase' => env('JWT_PASSPHRASE'),
+    ],
+
+    'ttl' => (int) env('JWT_TTL', 60),
+
+    'refresh_ttl' => (int) env('JWT_REFRESH_TTL', 20160),
+
+    'algo' => env('JWT_ALGO', 'HS256'),
+
+    'required_claims' => [
+        'iss',
+        'iat',
+        'exp',
+        'nbf',
+        'sub',
+        'jti',
+    ],
+
+    'persistent_claims' => [
+        //
+    ],
+
+    'lock__subject' => true,
+
+    'leeway' => (int) env('JWT_LEEWAY', 0),
+
+    'blacklist_enabled' => (bool) env('JWT_BLACKLIST_ENABLED', true),
+
+    'blacklist_grace_period' => (int) env('JWT_BLACKLIST_GRACE_PERIOD', 0),
+
+    'decrypt_cookies' => false,
+
+    'providers' => [
+        'jwt' => PHPOpenSourceSaver\JWTAuth\Providers\JWT\Namshi::class,
+        'auth' => PHPOpenSourceSaver\JWTAuth\Providers\Auth\Illuminate::class,
+        'storage' => PHPOpenSourceSaver\JWTAuth\Providers\Storage\Illuminate::class,
+    ],
+
+];
